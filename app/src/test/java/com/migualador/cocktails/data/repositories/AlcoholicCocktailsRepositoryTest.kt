13d184fdd2cca49cbc5ab2fc46256d5332613ff6
Mario Igualador <>
2025-09-13 19:26:58 +0200

    Replace mockito with mockk. Add more tests for alcoholic cocktails repository.

--- app/src/test/java/com/migualador/cocktails/data/repositories/AlcoholicCocktailsRepositoryTest.kt
+++ app/src/test/java/com/migualador/cocktails/data/repositories/AlcoholicCocktailsRepositoryTest.kt
@@ -6,143 +6,274 @@ import com.migualador.cocktails.data.datasources.alcoholic_cocktails.AlcoholicCo
 import com.migualador.cocktails.data.entities.Cocktail
 import com.migualador.cocktails.data.network.NetworkResult
 import com.migualador.cocktails.other.logging.Logger
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
+import org.junit.Before
 import org.junit.Test
-import org.mockito.kotlin.any
-import org.mockito.kotlin.doReturn
-import org.mockito.kotlin.mock
-import org.mockito.kotlin.never
-import org.mockito.kotlin.verifyBlocking
 
 
 class AlcoholicCocktailsRepositoryTest {
 
-    private val cocktailsList = listOf(Cocktail("id", "name", "imageUrl"))
+    private val cocktailsList = listOf(
+        Cocktail("id", "Afterglow", "https://www.thecocktaildb.com/images/0071_a.jpg"),
+        Cocktail("id", "Alice Cocktail", "https://www.thecocktaildb.com/images/897_b.jpg"),
+    )
 
-    private val logger: Logger = mock()
+    private val localDataSource = mockk<AlcoholicCocktailsLocalDataSource>(relaxed = true)
+    private val remoteDataSource = mockk<AlcoholicCocktailsRemoteDataSource>(relaxed = true)
+    private val dbDataSource = mockk<AlcoholicCocktailsDBDataSource>(relaxed = true)
+    private val logger = mockk<Logger>(relaxed = true)
 
-    private val mockEmptyAlcoholicCocktailsLocalDataSource: AlcoholicCocktailsLocalDataSource =
-        mock {
-            on { getAlcoholicCocktails()} doReturn emptyList()
-        }
+    private lateinit var repository: AlcoholicCocktailsRepository
 
-
-    private val mockNotEmptyAlcoholicCocktailsLocalDataSource: AlcoholicCocktailsLocalDataSource =
-        mock {
-            on { getAlcoholicCocktails() } doReturn cocktailsList
-        }
-
-    private val mockAlcoholicCocktailsRemoteDataSourceReturningSuccess: AlcoholicCocktailsRemoteDataSource =
-        mock {
-            onBlocking { getAlcoholicCocktails() } doReturn NetworkResult.Success(cocktailsList)
-        }
-
-
-    private val mockAlcoholicCocktailsRemoteDataSourceReturningHttpError: AlcoholicCocktailsRemoteDataSource =
-        mock {
-            onBlocking { getAlcoholicCocktails() } doReturn NetworkResult.HttpError(404)
-        }
-
-
-    private val mockAlcoholicCocktailsRemoteDataSourceReturningConnectionError: AlcoholicCocktailsRemoteDataSource =
-        mock {
-            onBlocking { getAlcoholicCocktails() } doReturn NetworkResult.ConnectionError()
-        }
-
-    private val mockEmptyAlcoholicCocktailsDBDataSource: AlcoholicCocktailsDBDataSource =
-         mock {
-            on { getAlcoholicCocktails()} doReturn emptyList()
-        }
-
-    private val mockNotEmptyAlcoholicCocktailsDBDataSource: AlcoholicCocktailsDBDataSource =
-        mock {
-            on { getAlcoholicCocktails()} doReturn cocktailsList
-        }
-
-
-
-    @Test
-    fun `When cocktails local datasource is not empty, data is not retrieved from either remote datasource or database datasource`() {
-        val repository = AlcoholicCocktailsRepository(
-            mockNotEmptyAlcoholicCocktailsLocalDataSource,
-            mockAlcoholicCocktailsRemoteDataSourceReturningSuccess,
-            mockNotEmptyAlcoholicCocktailsDBDataSource,
+    @Before
+    fun setUp() {
+        repository = AlcoholicCocktailsRepository(
+            localDataSource,
+            remoteDataSource,
+            dbDataSource,
             logger
         )
-
-        runBlocking {
-            repository.getAlcoholicCocktails()
-        }
-
-        verifyBlocking(mockAlcoholicCocktailsRemoteDataSourceReturningSuccess, never()) { getAlcoholicCocktails() }
-        verifyBlocking(mockNotEmptyAlcoholicCocktailsDBDataSource, never()) { getAlcoholicCocktails() }
     }
 
     @Test
-    fun `When cocktails local datasource is empty, data is retrieved from remote datasource`() {
-        val repository = AlcoholicCocktailsRepository(
-            mockEmptyAlcoholicCocktailsLocalDataSource,
-            mockAlcoholicCocktailsRemoteDataSourceReturningSuccess,
-            mockNotEmptyAlcoholicCocktailsDBDataSource,
-            logger
-        )
+    fun `When cocktails local datasource has data, repository retrieves data from local datasource`() {
+
+        coEvery {
+            localDataSource.getAlcoholicCocktails()
+        } returns cocktailsList
+
+        coEvery {
+            remoteDataSource.getAlcoholicCocktails()
+        } returns NetworkResult.Success(cocktailsList)
+
+        coEvery {
+            dbDataSource.getAlcoholicCocktails()
+        } returns cocktailsList
+
 
         runBlocking {
             repository.getAlcoholicCocktails()
         }
 
-        verifyBlocking(mockAlcoholicCocktailsRemoteDataSourceReturningSuccess) { getAlcoholicCocktails() }
+        coVerify(exactly = 1) { localDataSource.getAlcoholicCocktails() }
+    }
+
+    @Test
+    fun `When cocktails local datasource has data, repository does not retrieve data from remote datasource`() {
+        coEvery {
+            localDataSource.getAlcoholicCocktails()
+        } returns cocktailsList
+
+        coEvery {
+            remoteDataSource.getAlcoholicCocktails()
+        } returns NetworkResult.Success(cocktailsList)
+
+        coEvery {
+            dbDataSource.getAlcoholicCocktails()
+        } returns cocktailsList
+
+
+        runBlocking {
+            repository.getAlcoholicCocktails()
+        }
+
+        coVerify(exactly = 0) { remoteDataSource.getAlcoholicCocktails() }
+    }
+
+    @Test
+    fun `When cocktails local datasource has data, repository does not retrieve data from database datasource`() {
+        coEvery {
+            localDataSource.getAlcoholicCocktails()
+        } returns cocktailsList
+
+        coEvery {
+            remoteDataSource.getAlcoholicCocktails()
+        } returns NetworkResult.Success(cocktailsList)
+
+        coEvery {
+            dbDataSource.getAlcoholicCocktails()
+        } returns cocktailsList
+
+
+        runBlocking {
+            repository.getAlcoholicCocktails()
+        }
+
+        coVerify(exactly = 0) { dbDataSource.getAlcoholicCocktails() }
+    }
+
+    @Test
+    fun `When cocktails local datasource is empty and remote datasource returns Success, data is retrieved from remote datasource`() {
+        coEvery {
+            localDataSource.getAlcoholicCocktails()
+        } returns emptyList()
+
+        coEvery {
+            remoteDataSource.getAlcoholicCocktails()
+        } returns NetworkResult.Success(cocktailsList)
+
+        coEvery {
+            dbDataSource.getAlcoholicCocktails()
+        } returns cocktailsList
+
+
+        runBlocking {
+            repository.getAlcoholicCocktails()
+        }
+
+        coVerify(exactly = 1) { remoteDataSource.getAlcoholicCocktails() }
     }
 
     @Test
     fun `When cocktails local datasource is empty and remote datasource returns http error, data is retrieved from database datasource`() {
-        val repository = AlcoholicCocktailsRepository(
-            mockEmptyAlcoholicCocktailsLocalDataSource,
-            mockAlcoholicCocktailsRemoteDataSourceReturningHttpError,
-            mockNotEmptyAlcoholicCocktailsDBDataSource,
-            logger
-        )
+        coEvery {
+            localDataSource.getAlcoholicCocktails()
+        } returns emptyList()
+
+        coEvery {
+            remoteDataSource.getAlcoholicCocktails()
+        } returns NetworkResult.HttpError(404)
+
+        coEvery {
+            dbDataSource.getAlcoholicCocktails()
+        } returns cocktailsList
+
 
         runBlocking {
             repository.getAlcoholicCocktails()
         }
 
-        verifyBlocking(mockNotEmptyAlcoholicCocktailsDBDataSource) { getAlcoholicCocktails() }
+        coVerify(exactly = 1) { dbDataSource.getAlcoholicCocktails() }
     }
 
     @Test
     fun `When cocktails local datasource is empty and remote datasource returns connection error, data is retrieved from database datasource`() {
-        val repository = AlcoholicCocktailsRepository(
-            mockEmptyAlcoholicCocktailsLocalDataSource,
-            mockAlcoholicCocktailsRemoteDataSourceReturningConnectionError,
-            mockNotEmptyAlcoholicCocktailsDBDataSource,
-            logger
-        )
+        coEvery {
+            localDataSource.getAlcoholicCocktails()
+        } returns emptyList()
+
+        coEvery {
+            remoteDataSource.getAlcoholicCocktails()
+        } returns NetworkResult.ConnectionError()
+
+        coEvery {
+            dbDataSource.getAlcoholicCocktails()
+        } returns cocktailsList
+
 
         runBlocking {
             repository.getAlcoholicCocktails()
         }
 
-        verifyBlocking(mockNotEmptyAlcoholicCocktailsDBDataSource) { getAlcoholicCocktails() }
+        coVerify(exactly = 1) { dbDataSource.getAlcoholicCocktails() }
     }
 
     @Test
-    fun `When cocktails local datasource is empty and remote datasource returns Success, the data retrieved from remote datasource is added to local datasource and database datasource`() {
+    fun `When cocktails local datasource is empty and remote datasource returns Success, the data retrieved from remote datasource is set in local datasource`() {
 
-        val repository = AlcoholicCocktailsRepository(
-            mockEmptyAlcoholicCocktailsLocalDataSource,
-            mockAlcoholicCocktailsRemoteDataSourceReturningSuccess,
-            mockEmptyAlcoholicCocktailsDBDataSource,
-            logger
-        )
+        coEvery {
+            localDataSource.getAlcoholicCocktails()
+        } returns emptyList()
+
+        coEvery {
+            remoteDataSource.getAlcoholicCocktails()
+        } returns NetworkResult.Success(cocktailsList)
+
+        coEvery {
+            dbDataSource.getAlcoholicCocktails()
+        } returns emptyList()
+
+
         runBlocking {
             repository.getAlcoholicCocktails()
         }
 
-        verifyBlocking(mockEmptyAlcoholicCocktailsLocalDataSource) { setAlcoholicCocktails(any()) }
-        verifyBlocking(mockEmptyAlcoholicCocktailsDBDataSource) { setAlcoholicCocktails(any()) }
+        coVerify(exactly = 1) { localDataSource.setAlcoholicCocktails(cocktailsList) }
     }
 
+    @Test
+    fun `When cocktails local datasource is empty and remote datasource returns Success, the data retrieved from remote datasource is set in database datasource`() {
+
+        coEvery {
+            localDataSource.getAlcoholicCocktails()
+        } returns emptyList()
+
+        coEvery {
+            remoteDataSource.getAlcoholicCocktails()
+        } returns NetworkResult.Success(cocktailsList)
+
+        coEvery {
+            dbDataSource.getAlcoholicCocktails()
+        } returns emptyList()
 
 
+        runBlocking {
+            repository.getAlcoholicCocktails()
+        }
+
+        coVerify(exactly = 1) { dbDataSource.setAlcoholicCocktails(cocktailsList) }
+    }
+
+    @Test
+    fun `When cocktails local datasource is not empty, repository getAlcoholicCocktails returns the values contained in local data source`() {
+
+        val expectedResult = cocktailsList
+
+        coEvery {
+            localDataSource.getAlcoholicCocktails()
+        } returns cocktailsList
+
+        val result = runBlocking {
+            repository.getAlcoholicCocktails()
+        }
+
+        assert(result == expectedResult)
+    }
+
+    @Test
+    fun `When cocktails local datasource is empty and remote datasource returns Success, repository getAlcoholicCocktails returns the values contained in remote data source`() {
+
+        val expectedResult = cocktailsList
+
+        coEvery {
+            localDataSource.getAlcoholicCocktails()
+        } returns emptyList()
+
+        coEvery {
+            remoteDataSource.getAlcoholicCocktails()
+        } returns NetworkResult.Success(cocktailsList)
+
+        val result = runBlocking {
+            repository.getAlcoholicCocktails()
+        }
+
+        assert(result == expectedResult)
+    }
+
+    @Test
+    fun `When cocktails local datasource is empty and remote datasource returns http error and database datasource is not empty, repository getAlcoholicCocktails returns the values contained in database data source`() {
+
+        val expectedResult = cocktailsList
+
+        coEvery {
+            localDataSource.getAlcoholicCocktails()
+        } returns emptyList()
+
+        coEvery {
+            remoteDataSource.getAlcoholicCocktails()
+        } returns NetworkResult.HttpError(404)
+
+        coEvery {
+            dbDataSource.getAlcoholicCocktails()
+        } returns cocktailsList
+
+        val result = runBlocking {
+            repository.getAlcoholicCocktails()
+        }
+
+        assert(result == expectedResult)
+    }
 }
